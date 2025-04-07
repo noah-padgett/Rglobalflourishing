73fa944269c5d51c20309a2cbb284b4eed7cae17
noah-padgett <40502329+noah-padgett@users.noreply.github.com>
2025-04-07 14:17:34 -0400

    added quasi to binomial

--- R/attrition_wgts.R
+++ R/attrition_wgts.R
@@ -80,11 +80,11 @@ append_attrition_wgts <- function(data, obs.id.var = NULL, attr.pred = NULL, sta
   if (robust) {
     svy.data <- tmp.data
     fit.attr <- robustbase::glmrob(mod.form, data = svy.data,
-                                   family = stats::binomial(link = "logit"),
+                                   family = stats::quasibinomial(link = "logit"),
                                    weights = tmp.data[wgt])
   } else {
     fit.attr <- survey::svyglm(mod.form, design = svy.data,
-                               family = stats::binomial(link = "logit"),
+                               family = stats::quasibinomial(link = "logit"),
                                #family = stats::quasipoisson(link = "log"),
                                control = list(maxit = 1000))
   }
